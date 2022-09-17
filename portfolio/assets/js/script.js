7ac1109c71353e0027c37c18cae67e4d09d901f6
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-09-17 15:32:21 -0300

    ADD - Hamburguer Menu

---
+++ portfolio/assets/js/script.js
@@ -0,0 +1,32 @@
+// Replace the class "no-js" to "js"
+let $html = document.querySelector("html");
+
+$html.classList.remove("no-js");
+$html.classList.add("js");
+
+// Hamburguer Menu Logic
+let menuIsActive = false;
+
+let $hamburguer_menu = document.querySelector(".hamburguer-menu-icon");
+$hamburguer_menu.addEventListener("click", () => {
+    menuIsActive = !menuIsActive;
+    
+    let $menu = document.querySelector(".menu");
+    let $hamburguer_menu = document.querySelector(".hamburguer-menu-icon");
+    let $nav_menu = document.querySelector(".nav-page");
+    if(menuIsActive) {
+        $menu.style.backgroundImage = "var(--menu-background)";
+        $menu.style.boxShadow = "-2px 2px 4px rgba(0, 0, 0, .75)";
+        
+        $hamburguer_menu.src = "assets/images/icons/Close Icon.svg";
+        
+        $nav_menu.style.height = "auto";
+    } else {
+        $menu.style.backgroundImage = "none";
+        $menu.style.boxShadow = "none";
+        
+        $hamburguer_menu.src = "assets/images/icons/Hamburguer Menu Icon.svg";
+
+        $nav_menu.style.height = 0;
+    }
+});
